77f1739345e70f3134fae7bfb4ef4029f3576aa4
Janay Rawal <janayrawal@Janays-MacBook-Air.local>
2025-10-26 21:21:39 +0530

    Maths Bot final

--- MathsGPT/app.py
+++ MathsGPT/app.py
@@ -17,18 +17,18 @@ if not groq_api_key:
     st.info("Please add your GROQ API KEY to continue")
     st.stop()
 
-llm = ChatGroq(model="openai/gpt-oss-20b",groq_api_key=groq_api_key)
+llm = ChatGroq(model="qwen/qwen3-32b",groq_api_key=groq_api_key)
 
 #Intialize tools
 wikipedia_wrapper = WikipediaAPIWrapper()
 wikipedia_tool = Tool(
     name="Wikipedia",
-    func=wikipedia_wrapper.run(),
+    func=wikipedia_wrapper.run,
     description="A tool for searching the internet to find info on topic mentioned."
 )
 
 #Initialize Math Tool
-math_chain = LLMMathChain.from_llms(llm=llm)
+math_chain = LLMMathChain.from_llm(llm=llm)
 calculator = Tool(
     name = "Calculator",
     func = math_chain.run,
@@ -68,3 +68,26 @@ if "messages" not in st.session_state:
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg['content'])
+
+#function to generate response
+#def generate_response(question):
+#    response = assistant_agent.invoke({'input':question})
+#    return response
+
+#Intercation
+question = st.text_area("Enter your question:")
+if st.button("Find my answer"):
+    with st.spinner("Generating response..."):
+        if question:
+            st.session_state.messages.append({"role":"user","content":question})
+            st.chat_message("user").write(question)
+
+            st_cb = StreamlitCallbackHandler(st.container(),expand_new_thoughts=True)
+            response = assistant_agent.run(st.session_state.messages,callbacks=[st_cb])
+            st.session_state.messages.append({"role":"assistant","content":response})
+            st.write("Response:")
+            st.success(response)
+        else:
+            st.warning("Please enter the question")
+
+#I have 5 bananas and 7 grapes. I eat 2 bananas and give away 3 grapes. Then I buy a dozen of apples and 2 packs of blueberries. Each pack of blueberries contains 25 berries. How many total pieces of fruits do I have at the end?
